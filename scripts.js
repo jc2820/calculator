e8225e8a60a02028da66196d8be38ee1ca2209cf
Jamie Carter <jc2820@hotmail.com>
2019-10-24 15:51:08 +0100

    change const for variables to let
    Also change functions to es6 style.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 //new version
 
-var storage = {
+let storage = {
     displayValue: "",
     firstNum: null,
     secondNumReady: false,
     operator: null,
 };
 
-function input(num) {
-    const displayValue = storage.displayValue;
-    const secondNumReady = storage.secondNumReady;
+const input = (num) => {
+    let displayValue = storage.displayValue;
+    let secondNumReady = storage.secondNumReady;
     if (secondNumReady === true) {
         storage.displayValue = num;
         storage.secondNumReady = false;
@@ -18,24 +18,22 @@ function input(num) {
     }
   }
 
-function update() {
-    const display = document.querySelector('.screen');
+const update = () => {
+    let display = document.querySelector('.screen');
     display.value = storage.displayValue;
 }
 
-const keys = document.querySelector('.keys');
+let keys = document.querySelector('.keys');
 keys.addEventListener('click', (event) => {
-  const target = event.target;
+  let target = event.target;
   if (!target.matches('button')) {
     return;
   }
-
   if (target.classList.contains('operator')) {
     operatorInput(target.value);
     update();
     return;
   }
-
   if (target.classList.contains('decimal')) {
     if (!storage.displayValue.includes(target.value)) {
         storage.displayValue += target.value;
@@ -44,7 +42,6 @@ keys.addEventListener('click', (event) => {
     }
     return;
   }
-
   if (target.classList.contains('all-clear')) {
     storage.displayValue = "";
     storage.firstOperand = null;
@@ -53,32 +50,30 @@ keys.addEventListener('click', (event) => {
     update();
     return;
   }
-
   input(target.value);
     update();
 });
 
-function operatorInput(op) {
-    const firstNum = storage.firstNum
-    const displayValue = storage.displayValue
-    const operator = storage.operator
-    const inputValue = parseFloat(displayValue); //convert string to float!
+const operatorInput = (op) => {
+    let firstNum = storage.firstNum
+    let displayValue = storage.displayValue
+    let operator = storage.operator
+    let inputValue = parseFloat(displayValue); //convert string to float!
     if (firstNum === null) {
       storage.firstNum = inputValue;
     } else if (operator) {
-        const result = calculate(firstNum, inputValue); 
-        console.log(result);
-        displayValue = String(result);
-        firstNum = result;
+        let result = calculate(firstNum, inputValue); 
+        storage.displayValue = String(result);
+        storage.firstNum = result;
     }
     storage.operator = op;
     storage.secondNumReady = true;
     console.log(storage);
 }
 
-function calculate(a, b) {
-    const answer = "";
-    const operator = storage.operator;
+const calculate = (a, b) => {
+    let answer = "";
+    let operator = storage.operator;
     switch(operator) {
         case "+":
             answer = a + b;
@@ -95,8 +90,5 @@ function calculate(a, b) {
     return answer;
 }
 
-
-
-// add calculation function 
 // make equals button run calculaation function
 // make operators run calculation function on the fly.
