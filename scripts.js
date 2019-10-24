51f9cc0edf04e246c43dde48fe816a2be7738f48
Jamie Carter <jc2820@hotmail.com>
2019-10-24 13:13:29 +0100

    Add event listeners to console log buttons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
 //new version
+
+const keys = document.querySelector('.keys');
+keys.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
